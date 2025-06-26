1fc3dce79b662f9971b61ad64868b73555886ebf
Austin Gill <Austin.Gill@cnhind.com>
2025-06-26 14:41:26 -0500

    Add CLI arguments and token file reading

--- jobstats.py
+++ jobstats.py
@@ -13,7 +13,7 @@ def parse_args():
         "--log-level",
         "-l",
         type=str,
-        default="INFO",
+        default="DEBUG",
         choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
         help="Set the logging output level. Defaults to INFO.",
     )
@@ -25,11 +25,67 @@ def parse_args():
         help="Script output. Defaults to stdout.",
     )
 
+    group = parser.add_argument_group()
+
+    tokens = group.add_mutually_exclusive_group(required=True)
+    tokens.add_argument(
+        "--token",
+        "-t",
+        help="A GitLab PAT with API read access. Consider using --token-file instead",
+    )
+    tokens.add_argument(
+        "--token-file",
+        "-f",
+        type=argparse.FileType("r"),
+        help="Read the GitLab PAT from the given file",
+    )
+
+    group.add_argument(
+        "--domain",
+        "-d",
+        default="gitlab.cnh.com",
+        help="The domain for your GitLab instance. Defaults to gitlab.cnh.com",
+    )
+    group.add_argument(
+        "--project",
+        "-p",
+        required=True,
+        help="The full group/project path, or a project ID",
+    )
+    filter_limit = group.add_mutually_exclusive_group()
+    filter_limit.add_argument(
+        "--max-pipelines",
+        type=int,
+        default=2,  # TODO: Increase once finished
+        help="The maximum number of jobs to query",
+    )
+    filter_limit.add_argument(
+        "--since",
+        default=None,
+        help="An ISO-8061 date to query pipelines created since",
+    )
+    group.add_argument(
+        "--branch",
+        "-b",
+        default=None,
+        help="Filter jobs to the specified branch",
+    )
+
     return parser.parse_args()
 
 
 def main(args):
-    pass
+    token = get_token(args)
+
+
+def get_token(args) -> str:
+    """Get the GitLab PAT token using the method defined by the CLI args."""
+    if args.token:
+        return args.token
+    if args.token_file:
+        return args.token_file.read().strip()
+    # TODO: Add an option to read the token from stdin
+    raise RuntimeError("Failed to find GitLab PAT")
 
 
 if __name__ == "__main__":
